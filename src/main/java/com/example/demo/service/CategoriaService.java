15be945bb9d25d53a077a75a149d47f2de647964
Daniel Puma <dpumamendocilla@gmail.com>
2024-07-04 01:04:31 -0500

    Implementando el package Service

---
+++ src/main/java/com/example/demo/service/CategoriaService.java
@@ -0,0 +1,14 @@
+package com.example.demo.service;
+
+import java.util.List;
+
+import com.example.demo.entity.CategoriaEntity;
+
+public interface CategoriaService {
+
+	List<CategoriaEntity> findAllCategorias();
+
+	CategoriaEntity findCategoriaById(Integer id);
+
+	CategoriaEntity saveCategoria(CategoriaEntity categoria);}
+
---
+++ src/main/java/com/example/demo/service/ProductoService.java
@@ -0,0 +1,12 @@
+package com.example.demo.service;
+
+import com.example.demo.entity.ProductoEntity;
+import java.util.List;
+
+public interface ProductoService {
+
+	List<ProductoEntity> findAllProductos();
+	
+	ProductoEntity findProductoById(Integer id);
+
+	ProductoEntity saveProducto(ProductoEntity producto);}
