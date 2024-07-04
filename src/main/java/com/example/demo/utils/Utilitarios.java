fbf3a4329086e81c92245fbd208fa4f9ab3097f5
Daniel Puma <dpumamendocilla@gmail.com>
2024-07-04 00:59:43 -0500

    Implementando package Utils

---
+++ src/main/java/com/example/demo/utils/Utilitarios.java
@@ -0,0 +1,19 @@
+package com.example.demo.utils;
+import org.mindrot.jbcrypt.BCrypt;
+
+
+public class Utilitarios {
+
+	
+	
+	public static String extraerHash(String passwordInput) {
+		return BCrypt.hashpw(passwordInput, BCrypt.gensalt());
+	}
+	
+	
+	public static boolean checkPassword(String passwordInput, String hashPassword) {
+		return BCrypt.checkpw(passwordInput, hashPassword);
+	}
+	
+	
+	}
